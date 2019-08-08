1a2d522c03880f92d3a2d7f3dc2e5d7e103868fb
Vikas Sharma <vikas.sharma@quovantis.com>
2019-08-08 14:20:55 +0530

    sprint-item html modified

--- src/app/models/sprint-item.ts
+++ src/app/models/sprint-item.ts
@@ -3,6 +3,7 @@ import { SprintItemType } from './enums';
 export class SprintItem {
 
     id: number;
+    itemTitle: string;
     itemText: string;
     createdBy: number;
     createdOn: Date;
--- src/app/services/sprint-board/sprint-board.service.ts
+++ src/app/services/sprint-board/sprint-board.service.ts
@@ -14,6 +14,7 @@ export class SprintBoardService {
             id: 1,
             createdBy: 1,
             createdOn: new Date(),
+            itemTitle: 'title 1',
             itemText: 'some thing good',
             itemType: SprintItemType.WentWell
         },
@@ -21,13 +22,15 @@ export class SprintBoardService {
             id: 2,
             createdBy: 1,
             createdOn: new Date(),
+            itemTitle: 'title 2',
             itemText: 'some another thing happened',
             itemType: SprintItemType.WentWell
         },
         {
             id: 3,
             createdBy: 1,
-            createdOn: new Date(),
+            createdOn: new Date(),        
+            itemTitle: 'title 1',
             itemText: 'that was so hell of a sprint',
             itemType: SprintItemType.WentWell
         }
@@ -36,6 +39,7 @@ export class SprintBoardService {
             id: 3,
             createdBy: 1,
             createdOn: new Date(),
+            itemTitle: 'title 1',
             itemText: 'that was so hell of a sprint',
             itemType: SprintItemType.WentWell
         }
